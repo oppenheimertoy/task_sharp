0bb6bce045eeba835560bfb5a2b9a3064de25f1b
oppenheimertoy <titaninus4590@yandex.ru>
2022-11-28 00:26:45 +0300

    Added algorythms classes and Point implementation

---
+++ CLustAlgo/AbstarctAlgo.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Globalization;
+
+namespace CLustAlgo;
+
+abstract class AbstractAlgo
+{
+    private List<Point>? points;
+    // вынести как пример на принцип единственной ответсвенности
+    // public abstract void PrintToFile();
+	public abstract void PrintToConsole();
+    public List<Point>? GetPoints()
+    {
+        return this.points;
+    }
+}
+
+class Wave : AbstractAlgo
+{
+    private List<Point> points;
+    //private List<List<double>>? matrix; 
+    private double eps;
+
+    public Wave(List<Point> points, double eps)
+    {
+        this.points = points;
+        this.eps = eps;
+        //this.matrix = new List<List<double>>();
+    }
+
+    public override void PrintToConsole()
+    {
+        Console.WriteLine("rrfr");
+    }
+    public override void PrintToFile()
+    {
+        Console.WriteLine("rrfr");
+    }
+
+    public void FireNeighbours(int p_lable, int p_index)
+    {
+        if (this.points[p_index].GetLable() == 0)
+        {
+            this.points[p_index].SetClustFlag(p_lable);
+            for(int i = 0; i < this.points.Count; i++)
+            {
+                if (this.points[p_lable].Distance(this.points[i]) < eps && this.points[i].GetLable() == 0)
+                {
+                    p_index = i;
+                    FireNeighbours(p_lable, p_index);
+                }
+            }
+        }
+    }
+
+    public void InitWave()
+    {
+        int c_lab = 1;
+        int index = 0;
+        for(int i = 0; i < this.points.Count; i++) {
+            index = i;
+            FireNeighbours(c_lab, index);
+            c_lab++;
+        }
+        PrintToFile();
+    }
+    public bool CheckAllClusters()
+    {
+        int cnt = 0;
+        for (int i = 0; i < this.points.Count; i++)
+        {
+            if (this.points[i].GetLable() == 0)
+            {
+                cnt++;
+            }
+        }
+        if(cnt == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+
+class Hierarchical : AbstractAlgo
+{
+    private int num;
+    private List<Point> points;
+    private List<List<double>> matrix;
+
+    public Hierarchical(List<Point> points, int num)
+    {
+        this.points = points;
+        this.num = num;
+        this.matrix = new List<List<double>>();
+    }
+    public override void PrintToConsole()
+    {
+        Console.WriteLine("rrfr");
+    }
+    public override void PrintToFile()
+    {
+        Console.WriteLine("rrfr");
+    }
+    public void HierInit()
+    {
+        int lable = 1;
+        for(int i = 0; i < this.points.Count; ++i)
+        {
+            this.points[i].SetClustFlag(lable);
+            lable++;
+        }
+    }
+
+    public void FillMatrix()
+    {
+        for (int i = 0; i<= this.points.Count; ++i)
+        {
+            List<double> row = new List<double>();
+            this.matrix.Add(row);
+            for(int j = 0; j < this.points.Count; ++j)
+            {
+                this.matrix[i].Add(this.points[i].Distance(this.points[j]));
+            }
+
+        }
+    }
+
+    public int AmountOfClusters()
+    {
+        int cnt = 0;
+        List<int> p_points = new List<int>();
+        for (int i = 0; i < this.points.Count; ++i)
+        {
+            p_points.Add(this.points[i].GetLable());
+        }
+        p_points.Sort();
+        p_points.Distinct().ToList();
+        cnt = p_points.Count;
+        return cnt;
+    }
+
+    public void DeleteClusters(int lable)
+    {
+        for(int i = 0; i < this.points.Count; ++i)
+        {
+            for(int j = 0; j < this.points.Count; ++j)
+            {
+                if (this.points[i].GetLable() == lable && this.points[j].GetLable() == lable)
+                {
+                    this.matrix[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public void HierarchicalSearch()
+    {
+        FillMatrix();
+        HierInit();
+        int clusters = this.points.Count; 
+        while(clusters < num){
+            double min_d = 1000000;
+            int minind_1 = 0, minind_2 = 0;
+            for(int i = 0; i < this.points.Count; ++i)
+            {
+                for (int j = 0; j < this.points.Count; ++j)
+                {
+                    if ((this.matrix[i][j] < min_d) && (this.matrix[i][j] > 0))
+                    {
+                        min_d = this.matrix[i][j];
+                        minind_1 = i;
+                        minind_2 = j;
+                    }
+                }
+            }
+            int actlable = Math.Min(this.points[minind_1].GetLable(), this.points[minind_2].GetLable());
+            int oldlable = Math.Max(this.points[minind_1].GetLable(), this.points[minind_2].GetLable());
+            for (int i = 0; i < this.points.Count; ++i)
+            {
+                if (this.points[i].GetLable() == oldlable)
+                {
+                    this.points[i].SetClustFlag(actlable);
+                }
+            }
+            DeleteClusters(actlable);
+            clusters = AmountOfClusters();
+        }
+    }
+}
+
---
+++ CLustAlgo/Point.cs
@@ -0,0 +1,77 @@
+﻿namespace CLustAlgo;
+
+public class Point
+{
+    private double X;
+    private double Y;
+    private int с_flag;
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+        с_flag = 0;
+    }
+    public Point(double x, double y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+    public Point(double x, double y, int clust_flag) : this(x, y) { 
+        this.с_flag = clust_flag;
+    }
+
+    public Point(Point source_point)
+    {
+        this.X = source_point.X;
+        this.Y = source_point.Y;
+        this.с_flag = source_point.с_flag;
+    }
+
+    // setter block
+    public void SetClustFlag(int clust_flag) {
+        this.с_flag = clust_flag;
+    }
+    public void SetAccure(double pog1, double pog2)
+    {
+        this.X += pog1;
+        this.Y += pog2;
+    }
+
+    public void SetAccureX(double pog1)
+    {
+        this.X += pog1;
+    }
+
+    public void SetAccureY(double pog2)
+    {
+        this.Y += pog2;
+    }
+    // getters block 
+    public double GetDistanceZero()
+    {
+        return Math.Sqrt(X + Y);
+    }
+    public int GetLable()
+    {
+        return this.с_flag;
+    }
+    public double GetXCord()
+    {
+        return this.X;
+    }
+    public double GetYCord()
+    {
+        return this.Y;
+    }
+    public Tuple<double, double> GetCoord()
+    {
+        return new Tuple<double, double>(this.X, this.Y);
+    }
+
+    public double Distance(Point point)
+    {
+        return (Math.Sqrt(((X - point.X) * (X - point.X)) + ((Y - point.Y) * (Y - point.Y))));
+    } 
+
+}
